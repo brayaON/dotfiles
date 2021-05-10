0a8c0a3d2fd8162068a3529b402dc7ec3c925fb1
brayaON <bortizf@unal.edu.co>
2021-05-10 11:57:47 -0500

    Plugins

---
+++ vim/templates/tmp.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+const int MOD = 1000000007;
+
+int mod(int x, int m) { if (x < 0) x += m; return x % m; }
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    return 0;
+}
+
