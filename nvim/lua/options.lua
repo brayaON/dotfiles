a0c12b5dd19b10586933547f119ff8c0af508f36
brayaON <bof.23402@gmail.com>
2024-08-08 09:42:39 -0500

    Separate options to different file

---
+++ nvim/lua/options.lua
@@ -0,0 +1,37 @@
+-- Common vim.options configuration - B.O.F
+-- @brayaon20
+vim.g.mapleader = [[ ]] -- Space key is my leader
+
+vim.opt.background = 'dark'
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a' -- Enable mouse in all modes
+
+-- Sync OS and Neovim clipboards
+vim.schedule(function()
+    vim.opt.clipboard = 'unnamedplus'
+end)
+
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+
+-- Case-insensitive searching, unless the search contains a capital letter.
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Swap file is written to the disk after 250ms, instead of 4s.
+vim.opt.updatetime = 250
+
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- By default vsplit opens the tab on left and split opens the tab above.
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Preview substitutions as you type.
+vim.opt.inccommand = 'split'
+
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
