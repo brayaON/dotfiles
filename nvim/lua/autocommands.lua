b4f348837b29952c1bf29fa4bf70bafa353731e2
brayaON <bof.23402@gmail.com>
2024-08-08 09:41:49 -0500

    Separate autocmd statements to different file"

---
+++ nvim/lua/autocommands.lua
@@ -0,0 +1,16 @@
+-- Autocommands - B.O.F
+-- @brayaon20
+
+vim.api.nvim_create_autocmd("BufNewFile", {
+    pattern = "*.lua",
+    command = [[0read !printf "-- Description - B.O.F\n-- @brayaon20"]]
+})
+
+-- Highlight when yanking text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
