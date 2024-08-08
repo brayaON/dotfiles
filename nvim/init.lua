f182446e978d36d5806ec85249f86d8bb841e842
brayaON <bof.23402@gmail.com>
2024-08-08 09:43:12 -0500

    Refactor init file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,114 +1,42 @@
 -- Neovim configuration - B.O.F
 -- @brayaon20
 
--- Common options
-vim.g.mapleader = [[ ]]
+require('options')
 
-local opt = vim.opt
-opt.autoindent = true
-opt.background = "dark"
-opt.clipboard:append("unnamedplus")
-opt.number = true
-opt.relativenumber = true
-opt.shiftwidth = 4
-opt.smartindent = true
-opt.termguicolors = true
+require('keymaps')
 
--- Common mappings
-local keymap = vim.keymap
+require('autocommands')
 
--- Enclosing
-keymap.set('n', '+c', 'i"<Esc>ea"<Esc>') -- ""
-keymap.set('n', '+l', 'i{<Esc>ea}<Esc>') -- {}
-keymap.set('n', '+p', 'i(<Esc>ea)<Esc>') -- ()
+-- Check if Lazy is installed, otherwise install it
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.uv.fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
 
--- Saving & quitting
-keymap.set('n', '<leader>q', '<cmd>q<cr>')
-keymap.set('n', '<leader>w', '<cmd>w<cr>')
-
--- Tab movement
-keymap.set('n', '<leader>h', '<c-w>h')
-keymap.set('n', '<leader>j', '<c-w>j')
-keymap.set('n', '<leader>k', '<c-w>k')
-keymap.set('n', '<leader>l', '<c-w>l')
-
--- Buffer
-keymap.set('n', '<leader>1', '<cmd>b1<cr>')
-keymap.set('n', '<leader>2', '<cmd>b2<cr>')
-keymap.set('n', '<leader>3', '<cmd>b3<cr>')
-keymap.set('n', '<leader>4', '<cmd>b4<cr>')
-keymap.set('n', '<leader>d','<cmd>bd<cr>')
-
--- Misc
-keymap.set('n', '<leader>ñ','<cmd>noh<cr>')
-keymap.set('n', '<leader>g','<c-w>r')
-
--- Autocommands
-local api = vim.api
-
-api.nvim_create_autocmd("BufNewFile", {
-    pattern = "*.lua",
-    command = [[0read !printf "-- Description - B.O.F\n-- @brayaon20"]]
-})
-
--- api.nvim_create_autocmd("VimEnter", {
---     pattern = "*",
---     command = [[silent !tmux set status off]]
--- })
---
--- api.nvim_create_autocmd("VimLeave", {
---     pattern = "*",
---     command = [[silent !tmux set status on]]
--- })
-
--- Plugin setups
-require("plugins")
-
--- Indent Blankline
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#1F1F1F gui=nocombine]]
-require("ibl").setup()
-
--- Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+-- Set Up Lazy Plugins
+require("lazy").setup('plugins')
 
 -- Treesitter
 local treesitter = require('nvim-treesitter.configs')
 treesitter.setup {
     ensure_installed = { "cpp", "lua", "python", "vim", "yaml", "go", "terraform", "bash" },
-    highlight = {
-	enable = true
-    }
+    highlight = { enable = true }
 }
 
--- lsp-config
-local lsp = require('lsp-zero').preset({
-  name = 'minimal',
-  set_lsp_keymaps = true,
-  manage_nvim_cmp = true,
-  suggest_lsp_servers = false,
-})
-
-lsp.ensure_installed({
-    "clangd",
-    "lua_ls"
-})
-
-lsp.setup()
-
 -- Colorscheme
--- vim.g.material_style = "darker"
--- vim.g.everforest_background = 'med'
 vim.cmd [[ colorscheme everforest ]]
+-- vim.cmd [[ colorscheme kanagawa ]]
 
 -- Lualine
-require("lualine").setup()
-
--- Bufferline
-require("bufferline").setup()
+-- require("lualine").setup()
+--
+-- -- Bufferline
+-- require("bufferline").setup()
 
 -- Commenter
 require("Comment").setup()
