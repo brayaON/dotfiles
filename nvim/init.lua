8d294e6c662375064f0ab03850bed793a09358ad
brayaON <bof.23402@gmail.com>
2024-08-11 21:35:54 -0500

    Refactor main configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,7 +11,7 @@ require('autocommands')
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.uv.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     error('Error cloning lazy.nvim:\n' .. out)
   end
@@ -19,24 +19,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Set Up Lazy Plugins
-require("lazy").setup('plugins')
+require('lazy').setup('plugins')
 
--- Treesitter
-local treesitter = require('nvim-treesitter.configs')
-treesitter.setup {
-    ensure_installed = { "cpp", "lua", "python", "vim", "yaml", "go", "terraform", "bash" },
-    highlight = { enable = true }
-}
-
--- Colorscheme
-vim.cmd [[ colorscheme everforest ]]
--- vim.cmd [[ colorscheme kanagawa ]]
-
--- Lualine
--- require("lualine").setup()
---
--- -- Bufferline
--- require("bufferline").setup()
-
--- Commenter
-require("Comment").setup()
+-- vim.cmd([[ colorscheme everforest ]])
+vim.cmd([[ colorscheme kanagawa ]])
